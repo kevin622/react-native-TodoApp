468fc198ddbeba7f8719097818cd6e0f71debb2f
kevin622 <kevin622@yonsei.ac.kr>
2021-10-25 22:27:39 +0900

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View, TextInput, ScrollView } from 'react-native'; // SafeAreaView는 디바이스 크기를 인식해서 padding 적용하는 듯...?
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.appTitle}>Hello There</Text>
+      <View style={styles.card}>
+        <TodoInsert />
+        <TodoList />
+      </View>
+      {/* <StatusBar style="auto" /> */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#3143e8',
   },
+  appTitle: {
+    color: 'white',
+    fontSize: 36,
+    marginTop: 30,
+    marginBottom: 30,
+    fontWeight: '300',
+    textAlign: 'center',
+    backgroundColor: '#3143e8',
+  },
+  card: {
+    backgroundColor: '#fff',
+    marginLeft: 10,
+    marginRight: 10,
+    flex: 1,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: 1,
+    fontSize: 24,
+    marginLeft: 20,
+  }
 });
